c1b81b62b4cae5cfb4d2e729f716bf163cb78123
J2S <jon.sharp@j2ssolutions.com>
2014-10-29 05:57:21 +0000

    Update showhex.php

--- showhex.php
+++ showhex.php
@@ -1,19 +1,38 @@
-<?php
-print("hello\n");
-print("error log=".ini_get("error_log")."\n");
+<?php 
+print("Show un-printable characters in a string\n");
 
-$s= "abcd\t\nE";
+if (count($argv) == 1) {
+    $fp = fopen($argv[0],'r'); // will generate an error on failure
+} else {
+    $fp=STDIN;
+}
+
+if ($fp === FALSE) {
+    exit();
+}
+
+$s="abcd\t\nE";
 print showbin($s);
+
+//$stdin = fopen('php://stdin', 'r');
+while (($line = fgets($fp)) !== false) {
+   print showbin($line);
+}
+
+exit();
+
+/////////////////////////////////////////////
 
 function showbin($s) {
   $t='';
   for ($i=0; $i < strlen($s); $i++){
     $o = ord($s[$i]);
     if ($o > 127 || $o < 32) {
-      $t.='@'.bin2hex($s[$i]);
+      $t.='@('.bin2hex($s[$i]).')';
     } else {
       $t.=$s[$i];
     }
   }
   return $t;
 }
+
